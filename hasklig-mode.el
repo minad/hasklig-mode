18e028e15b1de3b1f66d9d549c394b8402aebfa5
Daniel Mendler <mail@daniel-mendler.de>
2025-01-01 10:21:24 +0100

    Update copyright years

--- hasklig-mode.el
+++ hasklig-mode.el
@@ -1,6 +1,6 @@
 ;;; hasklig-mode.el --- Hasklig ligatures -*- lexical-binding: t -*-
 
-;; Copyright (C) 2018-2024 Daniel Mendler
+;; Copyright (C) 2018-2025 Daniel Mendler
 
 ;; Author: Daniel Mendler
 ;; Created: 2018
